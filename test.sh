f6cbe1be186defc9d0de84a9a37e987b2ba2a94d
ajiLam918 <167869006+ajiLam918@users.noreply.github.com>
2024-06-23 07:30:41 +0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-mkdir /mnt/physics
+sudo mkdir /mnt/physics
 docker run -d --net=host -it -e NGROK_AUTHTOKEN=2eTzTtisRPj4pnUlPB0p8kGKdMR_5QHLfQkLt8qeaTwFDRnRP ngrok/ngrok:latest http --region ap 8006
 docker run -d -it --rm -p 8006:8006 -p 3389:3389 --device=/dev/kvm \
   -e VERSION='win10' -e RAM_SIZE='8G' -e CPU_CORES='2' \
