56d8e3bd799773bd89c7139e73b5d6d7e8c8d2e2
ajiLam918 <167869006+ajiLam918@users.noreply.github.com>
2024-06-24 06:25:57 +0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-sudo mkdir /mnt/physics
-docker run -d --net=host -it -e NGROK_AUTHTOKEN=2eTzTtisRPj4pnUlPB0p8kGKdMR_5QHLfQkLt8qeaTwFDRnRP ngrok/ngrok:latest http --region ap 8006
+
+docker run -d --net=host -it -e NGROK_AUTHTOKEN=2eTzTtisRPj4pnUlPB0p8kGKdMR_5QHLfQkLt8qeaTwFDRnRP ngrok/ngrok:latest tcp --region ap 3389
 docker run -d -it --rm -p 8006:8006 -p 3389:3389 --device=/dev/kvm \
-  -e VERSION='win10' -e RAM_SIZE='8G' -e CPU_CORES='2' \
-  -v /mnt/physics:/storage --cap-add NET_ADMIN --stop-timeout 120 dockurr/windows
+  -e VERSION='win10' -e RAM_SIZE='32G' -e CPU_CORES='16' \
+  --cap-add NET_ADMIN --stop-timeout 120 dockurr/windows
 echo "Success!"
